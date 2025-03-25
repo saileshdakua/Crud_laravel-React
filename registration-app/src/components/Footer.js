31bde52dc9f202712f8af087a02905c247f077d3
Sailesh Dakua <saileshdakua@gmail.com>
2025-03-25 07:39:12 +0530

    first commit

--- registration-app/src/components/Footer.js
+++ registration-app/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="py-4 bg-light mt-auto">
             <div className="container-fluid px-4">
                 <div className="d-flex align-items-center justify-content-between small">
-                    <div className="text-muted">Copyright &copy; Sailesh Dakua @ 2022</div>
+                    <div className="text-muted">Copyright &copy; Sailesh Dakua @ 2025</div>
                     <div>
                         <Link to="#">Privacy Policy</Link>
                         &middot;
